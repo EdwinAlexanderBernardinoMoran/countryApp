da7d8e0dea644b331aca6fd66d9487710768a6a8
Edwin Alexander Bernardino Moran <edwinalexanderbernardinomoran@gmail.com>
2023-11-07 09:53:19 -0600

    Mantener la data entre pantallas - Parte 1

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,12 +1,19 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Country } from '../interfaces/country';
+import { Country } from '../interfaces/country.interface';
 import { Observable, catchError, delay, map, of, tap } from 'rxjs';
+import { CacheStore } from '../interfaces/cache-store.interface';
 
 @Injectable({providedIn: 'root'})
 export class CountriesService {
 
   private apiUrl:string = 'https://restcountries.com/v3.1';
+
+  public cacheStore: CacheStore = {
+    byCapital: { term: '', countries:[]},
+    byCountries: { term: '', countries:[]},
+    byRegion: { region: '', countries:[]},
+  }
 
   constructor(private http: HttpClient) { }
 
